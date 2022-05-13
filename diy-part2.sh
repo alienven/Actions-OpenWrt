0e4e121bb05db99abe59cf0da1460befb49306d7
“Alienven” <alienven@qq.com>
2022-05-13 19:31:41 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,4 +20,4 @@ sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Make
 #cp -f openwrt/files/102-mt7621-fix-cpu-clk-add-clkdev.patch openwrt/target/linux/ramips/patches-5.4/102-mt7621-fix-cpu-clk-add-clkdev.patch
 
 #5.10超频到 1100Mhz
-cp -f openwrt/files/322-mt7621-fix-cpu-clk-add-clkdev.patch openwrt/target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+#cp -f openwrt/files/322-mt7621-fix-cpu-clk-add-clkdev.patch openwrt/target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
