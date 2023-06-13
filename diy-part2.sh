800524259b2e547434f5e21851cc3d39678a3557
“Alienven” <alienven@outlook.com>
2023-06-13 17:25:55 +0800

    update ufi

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 #5.14超频到 1100Mhz
-cp -f files/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4/102-mt7621-fix-cpu-clk-add-clkdev.patch
+#cp -f files/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4/102-mt7621-fix-cpu-clk-add-clkdev.patch
 
 #切换ramips内核到5.10
 #sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
